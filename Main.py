cedbb8da7ae96a8bf7cc8229fb5b30eda62e9668
Wyatt-Gold <103477172+Wyatt-Gold@users.noreply.github.com>
2024-09-22 19:09:50 -0500

    OOP Implemented
    Created a Game class that runs a game/round for the user. This class will be used to archive past games and act as a record for previous games played in the future. This included me adding a timer to see how long it took the user to complete the game, a counter of incorrect answers the user had, and new outputs at the end of the game. Game still console locked and only runs once. Made it to where game wont crash if user doesn't answer an equation with a valid integer.

--- Main.py
+++ Main.py
@@ -1,31 +1,80 @@
 # Will use tkinter when ready to make GUI for game
 # import tkinter as tk;
 
-#TODO Add stats: number of wrong asnwers and time
-#TODO Add different difficulties: How many problems, how many numbers, What kind of operators
+#TODO Create Game Loop: menu letting user determine what they want to do (Play game, See Past Games, etc)
+#TODO Add Different Difficulties: How many problems, how many numbers, What kind of operators
 #TODO Plan out GUI screens 
 #TODO Make the GUI for the game
 
 import random
+import time
 
-opeartions = ['+', '-', '*', "%"]
+class Game:
 
-NUM_PROBLEMS = 10
-# Generates an equation with 2 numbers and one operator
-def gen_equation():
-    operator = opeartions[random.randint(0,3)] # Creates randon number from 0 to 3 inclusive
-    num1 = random.randint(1, 20) 
-    num2 = random.randint(1, 20)
-    equation = str(num1) + " " + operator + " " + str(num2)
-    return equation, eval(equation) # Returns the equation in String form and answer to equation in int form
+    # Initializes an instance of a game 
+    def __init__(self, problems):
+        self.NUM_PROBLEMS = problems
+        self.operations = ['+', '-', '*', "%"]
+        self.num_incorrect = 0
+        self.start_time = None
+        self.end_time = None
+        self.history = ""
 
-problem_num = 0
-while problem_num <= NUM_PROBLEMS:
-    equation, answer = gen_equation()
-    while True:
-        response = input("Problem " + str(problem_num) + ": " + equation + " = ")
-        if(int(response) == answer):
-            break
-    problem_num += 1
+    # Getter Method: Retuns the number of incorrect answers a player had in the game
+    def get_num_incorrect(self):
+            return self.num_incorrect
+    
+    # Getter Method: Retuns the number of problems this game has
+    def get_num_problems(self):
+            return self.NUM_PROBLEMS
+    
+    # Getter Method: Returns how long it took the user to complete this game
+    def get_time(self):
+         return self.end_time - self.start_time
+    
+    # Getter Method: Retuns the documented history of the game (Each problem and answer user had)
+    def get_history(self):
+         return self.history
+    
+    # Used to make user play a game
+    def play_game(self):
+        self.start_time = time.time()
+        problem_num = 1
+        while problem_num <= self.NUM_PROBLEMS:
+            equation, answer = self.gen_equation()
+            while True:
+                problem = "Problem " + str(problem_num) + ": " + equation + " = "
+                response = input(problem)
+                self.history += problem + response + "\n"
+                try:
+                    if(int(response) == answer):
+                        break
+                    else:
+                        self.num_incorrect += 1
+                except ValueError: # User inputs an answer that is not a number
+                    self.num_incorrect += 1
 
-print("Congragulations! You've answered all the problems correctly!")
+            problem_num += 1
+        self.end_time = time.time()
+
+    # Used to generate equations for current game
+    def gen_equation(self):
+        operator = self.operations[random.randint(0,3)] # Creates randon number from 0 to 3 inclusive
+        num1 = random.randint(1, 20) 
+        num2 = random.randint(1, 20)
+        equation = str(num1) + " " + operator + " " + str(num2)
+        return equation, eval(equation) # Returns the equation in String form and answer to equation in int form
+
+
+## DRIVER CODE ##
+
+game1 = Game(problems=1)
+game1.play_game()
+
+print(f"Total elapsed time: {(game1.get_time()):.3f} seconds")
+num_problems = str(game1.get_num_problems())
+num_incorrect = str(game1.get_num_incorrect())
+print("Out of " + num_problems + (" problem," if int(num_problems) == 1 else " problems,")  + 
+      " you answered incorrectly " + num_incorrect + (" time!" if int(num_incorrect) == 1 else " times!"))
+print("\nGame History:")
+print(game1.get_history())
